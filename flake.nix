6216476fdeb717714dc23663460bbf025a868c46
Tristan Ross <tristan.ross@midstall.com>
2023-07-16 17:59:37 -0700

    chore: update deps

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
           src = cleanSource self;
 
           depsListFile = ./deps.json;
-          vendorHash = "sha256-Z8YR4QgVWOuB73Qsq5qlXr/SafR9FoZuYBpvyjQOvoo=";
+          vendorHash = "sha256-URWkC4Bj/mFN0tL3m5CWxcToDh8Pi7m/tfIHJZPWKP0=";
 
           flutterBuildFlags = [
             "--local-engine=${flutter-engine}/src/out/host_release"
