9746116e3344f1b9e73a5edd115a8690af513670
Tristan Ross <tristan.ross@midstall.com>
2025-01-03 14:09:28 -0800

    feat: add garnix config

--- flake.nix
+++ flake.nix
@@ -20,6 +20,11 @@
     };
   };
 
+  nixConfig = {
+    substituters = ["https://cache.garnix.io"];
+    trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  };
+
   outputs =
     {
       self,
@@ -158,6 +163,8 @@
                         enable = true;
                         wheelNeedsPassword = false;
                       };
+
+                      boot.initrd.kernelModules = [ "virtio_gpu" "virtio_pci" ];
 
                       virtualisation.qemu.options = [
                         "-vga none"
