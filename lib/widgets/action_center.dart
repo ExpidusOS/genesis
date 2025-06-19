1e565f7f09783d8fe479462281ba44ae318b5c5a
Tristan Ross <tristan.ross@midstall.com>
2025-06-19 10:02:37 -0700

    feat(action_center): make contents scrollable when too big

--- lib/widgets/action_center.dart
+++ lib/widgets/action_center.dart
@@ -43,82 +43,92 @@ class ActionCenter extends StatelessWidget {
             ),
           ],
         ),
-        FutureProvider(
-          initialData: null,
-          create: (_) =>
-              Provider.of<SystemServices>(context).createNetworking(),
-          dispose: (_, net) => net?.dispose(),
-          builder: (context, _) {
-            final net = Provider.of<NetworkingService?>(context);
-            if (net != null) {
-              return ListenableBuilder(
-                listenable: net,
-                builder: (context, _) => ListTileTheme(
-                  tileColor: Theme.of(context).colorScheme.background,
-                  child: Column(
-                    children: net.devices
-                        .where((dev) => dev.showInActionCenter)
-                        .map(
-                          (dev) => ListenableBuilder(
-                            listenable: dev,
-                            builder: (context, _) => Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: dev.wireless != null
-                                  ? ListenableBuilder(
-                                      listenable: dev.wireless!,
-                                      builder: (context, _) => ExpansionTile(
-                                        leading: Icon(dev.icon!),
-                                        title: Text(dev.iface),
-                                        trailing: Button(
-                                          isActive:
-                                              dev.status ==
-                                              NetworkDeviceStatus.active,
-                                          child: Icon(Icons.link_off),
-                                          onPressed: () {
-                                            dev.disconnect();
-                                          },
-                                        ),
-                                        children: [
-                                          SizedBox(
-                                            height: 250,
-                                            child: ListView(
-                                              children: dev
-                                                  .wireless!
-                                                  .accessPoints
-                                                  .map(
-                                                    (ap) => AccessPointTile(
-                                                      accessPoint: ap,
+        Expanded(
+          child: ListView(
+            children: [
+              FutureProvider(
+                initialData: null,
+                create: (_) =>
+                    Provider.of<SystemServices>(context).createNetworking(),
+                dispose: (_, net) => net?.dispose(),
+                builder: (context, _) {
+                  final net = Provider.of<NetworkingService?>(context);
+                  if (net != null) {
+                    return ListenableBuilder(
+                      listenable: net,
+                      builder: (context, _) => ListTileTheme(
+                        tileColor: Theme.of(context).colorScheme.background,
+                        child: Column(
+                          children: net.devices
+                              .where((dev) => dev.showInActionCenter)
+                              .map(
+                                (dev) => ListenableBuilder(
+                                  listenable: dev,
+                                  builder: (context, _) => Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: dev.wireless != null
+                                        ? ListenableBuilder(
+                                            listenable: dev.wireless!,
+                                            builder: (context, _) =>
+                                                ExpansionTile(
+                                                  leading: Icon(dev.icon!),
+                                                  title: Text(dev.iface),
+                                                  trailing: Button(
+                                                    isActive:
+                                                        dev.status ==
+                                                        NetworkDeviceStatus
+                                                            .active,
+                                                    child: Icon(Icons.link_off),
+                                                    onPressed: () {
+                                                      dev.disconnect();
+                                                    },
+                                                  ),
+                                                  children: [
+                                                    SizedBox(
+                                                      height: 250,
+                                                      child: ListView(
+                                                        children: dev
+                                                            .wireless!
+                                                            .accessPoints
+                                                            .map(
+                                                              (ap) =>
+                                                                  AccessPointTile(
+                                                                    accessPoint:
+                                                                        ap,
+                                                                  ),
+                                                            )
+                                                            .toList(),
+                                                      ),
                                                     ),
-                                                  )
-                                                  .toList(),
+                                                  ],
+                                                ),
+                                          )
+                                        : ActionRow(
+                                            start: Icon(dev.icon!),
+                                            title: dev.iface,
+                                            end: Button(
+                                              isActive:
+                                                  dev.status ==
+                                                  NetworkDeviceStatus.active,
+                                              child: Icon(Icons.link_off),
+                                              onPressed: () {
+                                                dev.disconnect();
+                                              },
                                             ),
                                           ),
-                                        ],
-                                      ),
-                                    )
-                                  : ActionRow(
-                                      start: Icon(dev.icon!),
-                                      title: dev.iface,
-                                      end: Button(
-                                        isActive:
-                                            dev.status ==
-                                            NetworkDeviceStatus.active,
-                                        child: Icon(Icons.link_off),
-                                        onPressed: () {
-                                          dev.disconnect();
-                                        },
-                                      ),
-                                    ),
-                            ),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-              );
-            }
-            return const CircularProgressIndicator();
-          },
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    );
+                  }
+                  return const CircularProgressIndicator();
+                },
+              ),
+            ],
+          ),
         ),
       ],
     ),
